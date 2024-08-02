5afd9f67d170191071bb9833acc5cd9ca283da3b
well016 <134378651+well016@users.noreply.github.com>
2024-08-02 17:34:21 +0300

    Delete Alien Invasion directory

--- Alien Invasion/hero.py
+++
@@ -1,8 +0,0 @@
-def hero(name, power, universe="DC"):
-    print(f"Name: {name}")
-    print(f"Power: {power}")
-    print(f"Universe: {universe}")
-
-
-hero("Superman", "flying")
-hero("Batman", "money", "DC")
