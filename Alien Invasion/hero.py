b08b0bfd3c4563c912788cb998b7c4334c833913
well016 <well016@mail.ru>
2024-08-02 15:35:22 +0300

    Добавлена функция для героя

--- Alien Invasion/hero.py
+++ Alien Invasion/hero.py
@@ -2,3 +2,7 @@ def hero(name, power, universe="DC"):
     print(f"Name: {name}")
     print(f"Power: {power}")
     print(f"Universe: {universe}")
+
+
+hero("Superman", "flying")
+hero("Batman", "money", "DC")
